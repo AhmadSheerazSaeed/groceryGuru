61d12ade6198be46a14b23c93a5dca350a8599d6
hiwi-dci-student <hiwot.abdi@dci.education>
2023-06-26 10:41:35 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import productRoutes from "./routes/productRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+
+import customerRouter from './route/customerRouter.js';
+import categoryRouter from './route/categoryRouter.js'
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+
+// routes:
+app.use('/api/customers', customerRouter);
+app.use('/api/category', categoryRouter);
+
+
+
+app.listen(process.env.PORT, (req, res) => {
+  console.log(`Server started on http://localhost:${process.env.PORT}`);
+});
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
+  )
+  .then(() => {
+    console.log(`Database ${process.env.DB_NAME} connected`);
+  })
+  .catch((error) => {
+    console.log(error.toString());
+  });
+
+app.use("/api/products", productRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/cart", cartRoutes);
