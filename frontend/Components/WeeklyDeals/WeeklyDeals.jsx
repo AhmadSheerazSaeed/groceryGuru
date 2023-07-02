1814a73f7ad5f54ef0cd93b2ae45834cd108b1dc
hiwi-dci-student <hiwot.abdi@dci.education>
2023-07-02 23:59:24 +0200

    responsive

--- frontend/Components/WeeklyDeals/WeeklyDeals.jsx
+++ frontend/Components/WeeklyDeals/WeeklyDeals.jsx
@@ -4,13 +4,16 @@ import Card from "./Card/Card.jsx";
 
 export default function WeeklyDeals() {
   return (
-    <div className="row" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' , margin:'0 15rem' }}>
-      <h1  style={{ margin: '20px auto', textAlign: 'center' }}>Weekly Deals </h1>
+    <>
+    <h1  style={{ margin: '20px auto', textAlign: 'center' }}>Weekly Deals </h1>
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' , margin:'0 15rem' }}>
+     
       {productData.map((product, key) => (
-        <div className="col-sm-4" key={key} style={{ marginBottom: '20px' }}>
+        <div className="col" key={key} style={{ marginBottom: '20px' }}>
           <Card product={product} />
         </div>
       ))}
     </div>
+    </>
   );
 }
