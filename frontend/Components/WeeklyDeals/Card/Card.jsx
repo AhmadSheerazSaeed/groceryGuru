1a52af7fe7df5acc4e2d15d88ee89af80ee0b2a7
hiwi-dci-student <hiwot.abdi@dci.education>
2023-07-05 14:11:39 +0200

    eslint off

--- frontend/Components/WeeklyDeals/Card/Card.jsx
+++ frontend/Components/WeeklyDeals/Card/Card.jsx
@@ -1,5 +1,5 @@
-//import React from 'react'
-import PropTypes from "prop-types";
+import React from 'react'
+
 import "./Card.css";
 
 export default function Card({ product }) {
@@ -9,7 +9,7 @@ export default function Card({ product }) {
       <div className="card" style={{ width: '23rem'  }}>
         <img src={product.imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">{product.itemName}</h5>
           <p className="card-text"> 
           Some quick example text to build on the card title and make up the bulk of the  content.
           </p>
@@ -21,8 +21,3 @@ export default function Card({ product }) {
     </>
   );
 }
-Card.propTypes = {
-  product: PropTypes.shape({
-    imageUrl: PropTypes.string.isRequired,
-  }).isRequired,
-};
--- frontend/Components/WeeklyDeals/WeeklyDeals.jsx
+++ frontend/Components/WeeklyDeals/WeeklyDeals.jsx
@@ -1,19 +1,27 @@
-
 import { productData } from "../../productData.js";
 import Card from "./Card/Card.jsx";
 
 export default function WeeklyDeals() {
   return (
     <>
-    <h1  style={{ margin: '20px auto', textAlign: 'center' }}>Weekly Deals </h1>
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' , margin:'0 15rem' }}>
-     
-      {productData.map((product, key) => (
-        <div className="col" key={key} style={{ marginBottom: '20px' }}>
-          <Card product={product} />
-        </div>
-      ))}
-    </div>
+      <h1 style={{ margin: "20px auto", textAlign: "center" }}>
+        Weekly Deals{" "}
+      </h1>
+      <div
+        className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4"
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          margin: "0 15rem",
+        }}
+      >
+        {productData.map((product, key) => (
+          <div className="col" key={key} style={{ marginBottom: "20px" }}>
+            <Card product={product} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
