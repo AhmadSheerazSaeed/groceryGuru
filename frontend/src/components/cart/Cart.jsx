56c83d9f690ced8392cef1bf8d0eb1220cf65fa4
Masahhud Larry <larrybarrister@yahoo.com>
2023-07-06 09:38:13 +0200

    images

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 
-function Cart() {
+import React from 'react';
+
+const Cart = ({ cartItems, removeFromCart }) => {
+  const handleRemoveFromCart = (product) => {
+    removeFromCart(product);
+  };
+
   return (
     <div>
-      <h1>This is CART component</h1>
+      <h2>Cart</h2>
+      {cartItems.length === 0 ? (
+        <p>Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              <img src={item.image} alt={item.name} />
+              <h3>{item.name}</h3>
+              <p>Price: {item.price} EUR</p>
+              <p>Quantity: {item.quantity}</p>
+              <button onClick={() => handleRemoveFromCart(item)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default Cart;
---
+++ frontend/src/components/products/Products.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Products = ({ selectedCategory, addToCart }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products based on selected category from the backend
+    if (selectedCategory) {
+      axios.get(`/api/products?categoryId=${selectedCategory}`)
+        .then((response) => {
+          setProducts(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching products:', error);
+        });
+    }
+  }, [selectedCategory]);
+
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.name} />
+          <h3>{product.name}</h3>
+          <p>Price: {product.price} EUR</p>
+          <button onClick={() => handleAddToCart(product)}>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
