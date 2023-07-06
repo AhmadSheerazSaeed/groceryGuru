636ce1356be1cb5e1030b872fde8cf4286db7562
hiwi-dci-student <hiwot.abdi@dci.education>
2023-07-06 13:01:45 +0200

    card sale css

--- frontend/src/components/WeeklyDeals/Card/Card.jsx
+++ frontend/src/components/WeeklyDeals/Card/Card.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
 
 import "./Card.css";
 
 export default function Card({ product }) {
   return (
     <>
-
-      <div className="card" >
+      <div className="card">
         <img src={product.imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{product.itemName}</h5>
-          <p className="card-text"> 
-          Some quick example text to build on the card title and make up the bulk of the  content.
+          <p className="card-text">
+            <span style={{ color: "#F39317" ,fontWeight: "bold"}} >&euro;{product.salePrice}</span>
+            <span style={{ textDecoration: "line-through" }}>
+              &euro;{product.purchasePrice}
+            </span>
           </p>
-          <a href="#" className="btn btn-primary"  style={{ backgroundColor: "#F39317", borderColor: "#F39317" }} >
-          Sale
+          <a
+            href="#"
+            className="btn btn-primary"
+            style={{ backgroundColor: "#F39317", borderColor: "#F39317" }}
+          >
+            Sale
           </a>
         </div>
       </div>
