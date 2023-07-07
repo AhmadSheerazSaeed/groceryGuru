fc6a3bb5bc7a9bf5dfb706f84d1ecdcd93055594
hiwi-dci-student <hiwot.abdi@dci.education>
2023-07-07 10:17:47 +0200

    fix category and cart bug

--- frontend/src/components/cart/Cart.jsx
+++ frontend/src/components/cart/Cart.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import React from 'react';
-
 const Cart = ({ cartItems, removeFromCart }) => {
   const handleRemoveFromCart = (product) => {
     removeFromCart(product);
