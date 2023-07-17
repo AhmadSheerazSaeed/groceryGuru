fa01214fb95b554e6b7a8135253709a80bb83c45
hiwi-dci-student <hiwot.abdi@dci.education>
2023-07-17 12:45:01 +0200

    css no sale

--- frontend/src/components/products/Card.jsx
+++ frontend/src/components/products/Card.jsx
@@ -2,20 +2,17 @@ import React from "react";
 
 import "./Card.css";
 
-
 export default function Card({ product }) {
   return (
     <>
       <div className="card">
         <img src={product.imageUrl} className="card-img-top" alt="..." />
-       
+
         <div className="card-body">
           <h5 className="card-title">{product.itemName}</h5>
           <p className="card-text">
-            <span style={{ color: "#F39317" ,fontWeight: "bold"}} >&euro;{product.salePrice}</span>
-            <span style={{ textDecoration: "line-through" }}>
-              &euro;{product.purchasePrice}
-            </span>
+            <span style={{ color: "#F39317", fontWeight: "bold" }}></span>
+            &euro;{product.purchasePrice}
           </p>
           <a
             href="#"
