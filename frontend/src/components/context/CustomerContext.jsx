74dfb57e39b287e44960b813d920f9c18fc2b6a4
AhmadSheerazSaeed <khera724@gmail.com>
2023-07-19 14:42:33 +0200

    added login and logout feature with displaying first name of the customer

--- frontend/src/components/context/CustomerContext.jsx
+++ frontend/src/components/context/CustomerContext.jsx
@@ -3,6 +3,7 @@ import { createContext, useState } from "react";
 const CustomerContext = createContext();
 
 const CustomerContextProvider = ({ children }) => {
+  // just to check, delete it later
   const [customerName, setCustomerName] = useState("Guest");
   return (
     <CustomerContext.Provider value={{ customerName, setCustomerName }}>
